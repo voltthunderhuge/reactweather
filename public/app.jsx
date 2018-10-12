b0195e30233341f977baa2ea85dc31d78f24a85c
Graeme Anderson <boltthunderhuge@hotmail.com>
2018-10-12 16:55:34 -0700

    nested components

--- public/app.jsx
+++ public/app.jsx
@@ -1,26 +1,87 @@
+var GreeterMessage = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <h1>Hello, {this.props.name}</h1>
+                <p>{this.props.message}</p>
+            </div>
+        );
+    }
+});
+
+
+var GreeterForm = React.createClass({
+    render: function() {
+        return (
+            <form onSubmit={this.onFormSubmit}>
+                <div>
+                    <input type="text" ref="name"/>
+                </div>
+                <div>
+                    <textarea ref="message" rows="10" cols="30"></textarea>
+                </div>
+                <div>
+                    <button>Press Me</button>
+                </div>
+            </form>
+        );
+    },
+    onFormSubmit: function(e) {
+        e.preventDefault();
+
+        const nameRef = this.refs.name;
+        const name = nameRef.value;
+
+        const messageRef = this.refs.message;
+        const message = messageRef.value;
+
+        var updates = {};
+        if (typeof name ==='string' && name.length > 0) {
+            updates.name = name;
+            nameRef.value = '';
+        }
+
+        if (typeof message === 'string' && message.length > 0) {
+            updates.message = message;
+            messageRef.value = '';
+        }
+
+        this.props.onNewFormData(updates);
+    }
+});
+
+
 var Greeter = React.createClass({
     getDefaultProps: function() {
         return {
             name: "React",
             message: "This is from the component"
-        }
+        };
+    },
+    getInitialState: function() {
+        return {
+            name: this.props.name,
+            message: this.props.message
+        };
+    },
+    handleNewFormData: function(updates) {
+        this.setState(updates);
     },
     render: function(){
-        var name = this.props.name;
-        var message = this.props.message;
-        
+
+        var name = this.state.name;
+        var message = this.state.message;
+
         return (
             <div>
-            <h1>Hello {name}</h1>
-            <p>
-                {message}
-            </p>
+                <GreeterMessage name={name} message={message}/>
+                <GreeterForm onNewFormData={this.handleNewFormData}/>
             </div>
-        )
+        );
     }
-})
+});
 
 ReactDOM.render(
-    <Greeter name="Graeme" message="Fuck yeah!"/>,
+    <Greeter name="Graeme"/>,
     document.getElementById("app")
-);  
+);
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+"use strict"
+
 var express = require("express");
 
 var app = express();
@@ -7,3 +9,10 @@ app.use(express.static("public"));
 app.listen(3000, function() {
     console.log("Express server listening on port 3000");
 });
+
+var aString = "a string";
+var anotherString = aString;
+
+aString = '';
+
+console.log(anotherString);
