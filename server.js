d4cffdf470d09151e19b8b7db121d9a01ebcb626
Graeme Anderson <boltthunderhuge@hotmail.com>
2018-10-18 14:31:17 -0700

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 "use strict"
 
 var express = require("express");
-
 var app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+    if (req.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        res.redirect(`http://${req.hostname}${req.url}`);
+    }
+});
 
 app.use(express.static("public"));
 
-app.listen(3000, function() {
-    console.log("Express server listening on port 3000");
+app.listen(port, function() {
+    console.log(`Express server listening on port ${port}`);
 });
